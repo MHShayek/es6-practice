1420e72a368724106b3c5b0747d8b7ed6234bb65
MHShayek <mahedihassanshayek@gmail.com>
2021-02-01 00:37:18 +0600

    again commited

--- letconst.js
+++ letconst.js
@@ -1,2 +1,21 @@
 const hubby = "Elias Kanchon";
 console.log(hubby);
+
+
+const numbers =[12 ,45];
+numbers[1] = 88;
+numbers.push(23);
+const nayok = {name:"sakib khan",phone: 4562};
+
+
+// console.log (numbers);
+
+let patientName ="Rahim chacha";
+patientName = "Fatema Khala";
+console.log (patientName);
+
+let sum = 0;
+for(var i= 0; i<10; i++){
+    sum = sum +i;
+}
+console.log(i);
